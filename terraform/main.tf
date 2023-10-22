987864fafbea0d072606cf16b305f681b58d6c3a
alyamani147 <mohamadalyamani@hotmail.com>
2023-10-22 13:55:42 +0300

    Removed a tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "ghost" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
+      private_key = file("${github.workspace}/id_rsa")
       host        = self.public_ip
     }
   }
